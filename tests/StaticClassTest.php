c3b9d0f73e4ca79460580c27bf72273ed5dc6201
Valentin Jebelev <vala.jebelev@gmail.com>
2022-11-24 00:11:40 +0200

    Fix test expects incorrect exception

--- tests/StaticClassTest.php
+++ tests/StaticClassTest.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Devly\Utils\Tests;
 
 use Devly\Utils\Tests\Fake\StaticFoo;
-use Error;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class StaticClassTest extends TestCase
 {
     public function testInitiationThrowsException(): void
     {
-        $this->expectException(Error::class);
+        $this->expectException(Exception::class);
 
         new StaticFoo();
     }
